674d63f156cf295326b2e54850a089f73d5082ca
Pető Zoltán <bajgli@gmail.com>
2018-07-11 15:45:22 +0200

    show raw/signed transaction

--- app/scripts/controllers/betrPlaceBetCtrl.js
+++ app/scripts/controllers/betrPlaceBetCtrl.js
@@ -1,5 +1,6 @@
 'use strict';
 var betrPlaceBetCtrl = function($scope, $sce, walletService) {
+    walletService.wallet = null
     $scope.visibility = 'interactView'
     $scope.sendContractModal = new Modal(document.getElementById('sendContract'))
     $scope.wallet = walletService.wallet
@@ -10,9 +11,18 @@ var betrPlaceBetCtrl = function($scope, $sce, walletService) {
     } catch(e) {
         $scope.notifier.danger(e)
     }
+    
     $scope.data = qs.params[0].data
     $scope.to = qs.params[0].to
-
+    $scope.tx = {
+        gasLimit: '',
+        data: qs.params[0].data,
+        to: qs.params[0].to,
+        unit: "ether",
+        value: 0,
+        nonce: null,
+        gasPrice: null
+    }
     $scope.generateContractTx = function() {
         $scope.wd = true
         $scope.wallet = walletService.wallet
@@ -20,8 +30,27 @@ var betrPlaceBetCtrl = function($scope, $sce, walletService) {
     }
 
     $scope.generateTx = function() {
-        if ($scope.wallet == null) throw 'No wallet found';
-        console.log($scope.wallet)
+        try {
+            if ($scope.wallet == null) throw globalFuncs.errorMsgs[3]
+        } catch (e) {
+            $scope.notifier.danger(e);
+        }
+        $scope.tx.contractAddr = qs.params[0].to
+        $scope.tx.data = ethFuncs.sanitizeHex($scope.tx.data)
+        var txData = uiFuncs.getTxData($scope)
+        console.log(txData)
+        uiFuncs.generateTx(txData, function(rawTx) {
+            if (!rawTx.isError) {
+                $scope.rawTx = rawTx.rawTx
+                $scope.signedTx = rawTx.signedTx
+                $scope.showRaw = true
+            } else {
+                $scope.showRaw = false
+                $scope.notifier.danger(rawTx.error)
+            }
+            if (!$scope.$$phase) $scope.$apply()
+        })
+
     }
 }
 
