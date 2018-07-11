429d64a1ee8777ce763985597bba25310ddba5f5
Pető Zoltán <bajgli@gmail.com>
2018-07-11 14:23:17 +0200

    wallet instance is present on send tx page

--- app/scripts/controllers/betrPlaceBetCtrl.js
+++ app/scripts/controllers/betrPlaceBetCtrl.js
@@ -2,7 +2,8 @@
 var betrPlaceBetCtrl = function($scope, $sce, walletService) {
     $scope.visibility = 'interactView'
     $scope.sendContractModal = new Modal(document.getElementById('sendContract'))
-    
+    $scope.wallet = walletService.wallet
+
     var qs = globalFuncs.urlGet('qs') == null ? "" : globalFuncs.urlGet('qs')
     try {
         qs = JSON.parse(qs.replace(/\'/g, '\"'))
@@ -14,7 +15,13 @@ var betrPlaceBetCtrl = function($scope, $sce, walletService) {
 
     $scope.generateContractTx = function() {
         $scope.wd = true
+        $scope.wallet = walletService.wallet
         $scope.sendContractModal.open()
+    }
+
+    $scope.generateTx = function() {
+        if ($scope.wallet == null) throw 'No wallet found';
+        console.log($scope.wallet)
     }
 }
 
